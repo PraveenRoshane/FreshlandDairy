112cbe6e69b463d482fe2fc9469be244fcf0fb90
PraveenRoshane <praveenroshane123@gmail.com>
2021-10-03 16:51:34 +0530

    shop order update

--- FreshlandDairy/src/main/java/com/main/backend/FreshlandDairy/controller/ShopOrderDetailsController.java
+++ FreshlandDairy/src/main/java/com/main/backend/FreshlandDairy/controller/ShopOrderDetailsController.java
@@ -1,21 +1,16 @@
 package com.main.backend.FreshlandDairy.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.main.backend.FreshlandDairy.entity.ShopOrderDetails;
 import com.main.backend.FreshlandDairy.entity.ShopOrderDetailsPK;
 import com.main.backend.FreshlandDairy.entity.ShopOrderReciver;
@@ -29,23 +24,22 @@ public class ShopOrderDetailsController {
 	private ShopOrderDetailsRepository ShopOrderRepository;
 	
 	@GetMapping("/shop-order/orderdetails")
-	public List<ShopOrderDetails> getALLOrders() {
+	public List<ShopOrderDetails> getALLOrdersDetails() {
 		return ShopOrderRepository.findAll();
 	}
 	
 //	@GetMapping("/shop-order/orderdetails/{id}")
-//	public List<ShopOrderDetails> getByTransactionID(@PathVariable Long transactionID) {
-//		return ShopOrderRepository.findByTransactionID(transactionID);
+//	public List<ShopOrderDetails> getOrderDetails(@PathVariable("id") Long id) {
+//		return ShopOrderRepository.findByTransactionID(id);
 //	}
 	
-//	@DeleteMapping("/shop-order/orderdetails/{id}")
-//	public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
-//
-//		ShopOrderRepository.deleteById(id);
-//
-//		return ResponseEntity.noContent().build();
-//	}
-//
+	@DeleteMapping("/shop-order/orderdetails/delete")
+	public ResponseEntity<Void> deleteOrder(@RequestBody ShopOrderDetailsPK primarykey) {
+		System.out.println(primarykey);
+		ShopOrderRepository.deleteById(primarykey);
+		return ResponseEntity.noContent().build();
+	}
+
 //	@PutMapping("/shop-order/orderdetails/{id}")
 //	public ResponseEntity<ShopOrderDetails> updateOrder(@PathVariable Long id, @RequestBody ShopOrderDetails order) {
 //		
